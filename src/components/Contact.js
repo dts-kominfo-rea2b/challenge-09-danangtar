02f549412c9b70006d85c748b6cc077ca05552a0
Danang Adi Nugroho <danangtar@gmail.com>
2022-06-19 22:42:06 +0700

    Add react list component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (param) => {
     return (
-        <></>
+        <div className="contact">
+            <div className="kitten">
+                <img src={param.data.photo}></img>
+            </div>
+            <div className="info">
+                <p className="name">{param.data.name}</p>
+                <p>{param.data.phone}</p>
+                <p>{param.data.email}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <div id="header">
+            <h1>Call a Friend</h1>
+            <h2>your friendly contact app</h2>
+        </div>
     )
 }
 
